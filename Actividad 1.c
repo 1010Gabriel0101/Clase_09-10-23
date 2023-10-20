e85b006bbcb32e7797ea4bd15f9c904e2d3e9908
1010Gabriel0101 <148381895+1010Gabriel0101@users.noreply.github.com>
2023-10-19 22:43:43 -0600

    Update Actividad 1.c

--- Actividad 1.c
+++ Actividad 1.c
@@ -7,4 +7,31 @@ for(x = 0; x <= 10; x++){
    y = cuadrado(x);
    printf("%d"\n,y);
 }
-
+}
+int cuadrado(int y){
+   return y*y;
+}
+int maximo(int x, int y, int z)
+int max = x;
+if(y > max)
+   max = y ;
+if(z > max)
+   max = z;
+return max;
+}
+int maximo_array(int x[],int tamaño){
+   int max = 0;
+   for(int i = 0; i < tamaño; i++){
+      if(max < x[i]){
+         max = x[i];
+      }
+   }
+   return max;
+}
+int factorial(int x){
+   int fact = x;
+   for(int i = x; i >= 1; i--){
+      fact = fact*i-1;
+   }
+   return fact;
+}
